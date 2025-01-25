0c46c20be482338a374920a4691c029696312e89
kamal Prajapat <kamalprajapat7117@gmail.com>
2025-01-25 22:32:24 +0530

    add code of Interleave

---
+++ Queue/interleaveQueue/interleaveQueueImplematation.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class interleaveQueueImplematation{
+public static void QueueC(Queue<Integer> q){
+    int n = q.size();
+    Queue<Integer> first = new LinkedList<>();
+
+    for(int i = 0;i<n/2;i++){
+        first.add(q.remove());
+    }
+while(!first.isEmpty()){
+    q.add(first.remove());
+    q.add(q.remove());
+}
+}
+
+    public static void main(String[] args){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+
+        QueueC(q);
+        
+            System.out.println(q);
+        
+    }
+}

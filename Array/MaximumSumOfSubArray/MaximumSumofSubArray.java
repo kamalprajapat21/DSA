6df539df19c339514f81d399b71342f225e2f0a7
kamal Prajapat <kamalprajapat7117@gmail.com>
2025-02-14 00:06:01 +0530

    Add MaximumSumOfSubArray

---
+++ Array/MaximumSumOfSubArray/MaximumSumofSubArray.java
@@ -0,0 +1,17 @@
+public class MaximumSumofSubArray{
+
+    static int Solution(int arr[]){
+        int n = arr.length;
+        int csum = 0;
+        int msum = 0;
+        for(int i = 0;i<n-1;i++){
+            csum = arr[i] + arr[i+1];
+            msum = Math.max(csum,msum);
+        }
+        return msum;
+    }
+    public static void main(String[]args){
+        int arr[] = {4, 3, 1, 5, 6};
+        System.out.println(Solution(arr));
+    }
+}
